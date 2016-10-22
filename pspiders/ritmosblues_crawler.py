2b83259732f3ae3404629357bcd5c6eb31357d28
LLCampos <luis1212@sapo.pt>
2016-10-22 19:41:34 +0100

    add crawler for ritmos e blues

---
+++ pspiders/ritmosblues_crawler.py
@@ -0,0 +1,25 @@
+import scrapy
+
+
+class RitmosBlues(scrapy.Spider):
+    name = 'ritmos_e_blues'
+    start_urls = ['http://www.ritmoseblues.pt/passatempos/']
+
+    domain = 'http://www.ritmoseblues.pt'
+
+    def parse(self, response):
+
+        passatempos_query = '//div[@class="center-wraper"]/table'
+
+        for passatempo in response.xpath(passatempos_query):
+
+            passatempo_name = passatempo.xpath('.//span[@class="event-sub-tituloW"]/text()').extract_first()
+            # href is this page is relative, so I need to add the domain
+            # also, the href in this page is like this: ../bieber/passatempo/",
+            # so I take off the two points
+            passatempo_link = RitmosBlues.domain + \
+                passatempo.xpath('.//table//a/@href').extract_first()[2:]
+
+            yield {
+                passatempo_name: passatempo_link
+            }
