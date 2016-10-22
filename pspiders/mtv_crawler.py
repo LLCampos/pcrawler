a7aec9663deb03dcf3ed3e89395b561a639204a3
LLCampos <luis1212@sapo.pt>
2016-10-22 17:38:46 +0100

    create mtv_crawler

---
+++ pspiders/mtv_crawler.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class MTVCrawler(scrapy.Spider):
+    name = 'hollywood'
+    start_urls = ['http://mtv.pt/passatempos/']
+
+    domain = 'http://mtv.pt'
+
+    def parse(self, response):
+
+        passatempos_query = '//div[@data-mtv-module-shortid="nu7kqa"]/div//a'
+
+        for passatempo in response.xpath(passatempos_query):
+
+            passatempo_name = passatempo.xpath("div/h3/span/text()").extract_first()
+
+            # href is this page is relative, so I need to add the domain
+            passatempo_link = MTVCrawler.domain + \
+                passatempo.xpath('@href').extract_first()
+
+            yield {
+                passatempo_name: passatempo_link
+            }
