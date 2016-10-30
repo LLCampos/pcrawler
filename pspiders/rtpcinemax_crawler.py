133c179af9c492b2ac32ac1e69d5b1fb552b240e
LLCampos <luis1212@sapo.pt>
2016-10-30 20:23:21 +0000

    add rtp cinemax crawler

---
+++ pspiders/rtpcinemax_crawler.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class RTPCinemaxCrawler(scrapy.Spider):
+    name = 'rtpcinemax'
+    start_urls = ['http://www.rtp.pt/cinemax/?headline=23']
+
+    domain = 'http://www.rtp.pt'
+
+    def parse(self, response):
+
+        passatempos_query = '//ul/li/div/div[1]/a'
+
+        for passatempo in response.xpath(passatempos_query):
+
+            passatempo_name = passatempo.xpath('@title').extract_first()
+            # href is this page is relative, so I need to add the domain
+            passatempo_url = RTPCinemaxCrawler.domain + \
+                passatempo.xpath('@href').extract_first()
+
+            yield {
+                "name": passatempo_name,
+                "url": passatempo_url
+            }
