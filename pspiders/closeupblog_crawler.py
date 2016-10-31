b4d3b2bee117a7e0d355e05e38a2df596c483e47
LLCampos <luis1212@sapo.pt>
2016-10-31 16:16:23 +0000

    add closeupblog crawler

---
+++ pspiders/closeupblog_crawler.py
@@ -0,0 +1,20 @@
+import scrapy
+
+
+class CloseUpBlogCrawler(scrapy.Spider):
+    name = 'closeupblog'
+    start_urls = ['http://close-up-blog.blogspot.pt/p/passatempos-decorrer.html']
+
+    def parse(self, response):
+
+        passatempos_query = '//div[@class="separator"]/b/a'
+
+        for passatempo in response.xpath(passatempos_query):
+
+            passatempo_name = passatempo.xpath('span/text()').extract_first()
+            passatempo_url = passatempo.xpath('@href').extract_first()
+
+            yield {
+                "name": passatempo_name,
+                "url": passatempo_url
+            }
