910b75fb0652da809c0e537c3ba685b6418f7aa5
LLCampos <luis1212@sapo.pt>
2016-10-31 15:59:23 +0000

    add splitscreen crawler

---
+++ pspiders/splitscreen_crawler.py
@@ -0,0 +1,26 @@
+import scrapy
+
+
+class SplitscreenCrawler(scrapy.Spider):
+    name = 'splitescreen'
+    start_urls = ['http://splitscreen-blog.blogspot.pt/p/passatempos.html']
+
+    def parse(self, response):
+
+        passatempos_query = '//div[@class="post-body entry-content"]/div[2]/div[2]/div'
+        names = response.xpath(passatempos_query + '/b')
+        urls = response.xpath(passatempos_query + '/a')
+
+        if len(names) != len(urls):
+            raise ValueError('There\'s something wrong '
+                             'in the xpath expressions')
+
+        for i in range(len(names)):
+
+            passatempo_name = names[i].extract()
+            passatempo_url = urls[i].extract()
+
+            yield {
+                "name": passatempo_name,
+                "url": passatempo_url
+            }
