7ac8e9b8311be130dc79c468de3ecd19390f3bb5
LLCampos <luis1212@sapo.pt>
2017-07-15 18:12:15 +0100

    Fix RTP bot

--- pspiders/rtpcinemax_crawler.py
+++ pspiders/rtpcinemax_crawler.py
@@ -5,8 +5,6 @@ class RTPCinemaxCrawler(scrapy.Spider):
     name = 'rtpcinemax'
     start_urls = ['http://www.rtp.pt/cinemax/?headline=23']
 
-    domain = 'http://www.rtp.pt'
-
     def parse(self, response):
 
         passatempos_query = '//ul/li/div/div[1]/a'
@@ -15,8 +13,7 @@ class RTPCinemaxCrawler(scrapy.Spider):
 
             passatempo_name = passatempo.xpath('@title').extract_first()
             # href is this page is relative, so I need to add the domain
-            passatempo_url = RTPCinemaxCrawler.domain + \
-                passatempo.xpath('@href').extract_first()
+            passatempo_url = passatempo.xpath('@href').extract_first()
 
             yield {
                 "name": passatempo_name,
