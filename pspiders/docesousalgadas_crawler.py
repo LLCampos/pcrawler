4415d53fffd4bdf61db1f829420bfb2f614c3c79
LLCampos <luis1212@sapo.pt>
2017-08-05 19:54:01 +0100

    Fix DocesOuSalgadas bot

--- pspiders/docesousalgadas_crawler.py
+++ pspiders/docesousalgadas_crawler.py
@@ -3,25 +3,20 @@ from lxml import etree
 
 
 class DocesOuSalgadasCrawler(scrapy.Spider):
-    '''Here I scrap the RSS feed. It's easier because the site is based on
-    Javascript.'''
     name = 'docesousalgadas'
-    start_urls = ['https://docesousalgadas.blogspot.pt//feeds/posts/default']
+    start_urls = ['http://docesousalgadas.pt/category/passatempos/']
 
     def parse(self, response):
 
-        namespace = 'http://www.w3.org/2005/Atom'
-        tree = etree.fromstring(response.text.encode('utf-8'))
+        passatempos_query = '//h2[@class="entry-title"]/a'
 
-        passatempos_query = 'x:entry[x:title[contains(text(), "Passatempo")]]'
-        passatempos = tree.xpath(passatempos_query, namespaces={'x': namespace})
+        for passatempo in response.xpath(passatempos_query):
 
-        for passatempo in passatempos:
-
-            passatempo_name = passatempo.xpath('x:title/text()', namespaces={'x': namespace})[0]
-            passatempo_url = passatempo.xpath('x:link[@rel="alternate"]/@href', namespaces={'x': namespace})[0]
+            passatempo_name = passatempo.xpath('text()').extract_first()
+            passatempo_url = passatempo.xpath('@href').extract_first()
 
             yield {
                 "name": passatempo_name,
                 "url": passatempo_url
             }
+
