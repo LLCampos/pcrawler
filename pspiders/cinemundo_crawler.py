d758311f5e021a0bf454babaa0fc49889c0d30bd
LLCampos <luis1212@sapo.pt>
2017-08-06 11:18:33 +0100

    Fix Cinemundo scraper

--- pspiders/cinemundo_crawler.py
+++ pspiders/cinemundo_crawler.py
@@ -7,7 +7,7 @@ class CinemundoCrawler(scrapy.Spider):
 
     def parse(self, response):
 
-        passatempos_query = '//div[p[not(contains(text(), "Terminado")) and not(contains(text(), "terminado"))] and @class="col-md-6 text-normal element-top-0 element-bottom-20 text-center"]//a'
+        passatempos_query = '//div[@class="col-md-6 text-normal element-top-0 element-bottom-20 text-center"]//a'
 
         for passatempo in response.xpath(passatempos_query):
 
